e25a40b6c8af390e5a2f52052f5901c0979599b1
konflic <konflic@mail.ru>
2021-07-11 11:37:06 +0300

    Версия 2.3

--- test_example/test_using_iterator.py
+++ test_example/test_using_iterator.py
@@ -5,7 +5,7 @@ from csv import DictReader
 from csv import reader
 
 
-def _method(method_string):
+def request_method(method_string: str):
     """Transform string to requests method"""
     return requests.get if method_string.lower() == "get" else requests.post
 
@@ -16,7 +16,11 @@ data_dictreader = DictReader(open("test_data.csv"))
 
 @pytest.mark.parametrize("entry", data_dictreader)
 def test_dict_reader(entry):
-    assert _method(entry['method'])(entry["url"], allow_redirects=False).status_code == int(entry["status"])
+    method = entry['method']
+    url = entry["url"]
+    expected_status = int(entry["status"])
+
+    assert request_method(method)(url, allow_redirects=False).status_code == expected_status
 
 
 # Getting data from csv file with reader
@@ -26,4 +30,4 @@ header = next(data_reader)
 
 @pytest.mark.parametrize(header, data_reader)
 def test_reader(url, method, status):
-    assert _method(method)(url, allow_redirects=False).status_code == int(status)
+    assert request_method(method)(url, allow_redirects=False).status_code == int(status)
